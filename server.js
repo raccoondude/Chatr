29eaa4e78753c23440b4ff024a491d18158e3cd8
Glitch (parallel-crowberry) <none>
2019-08-18 22:11:42 +0000

    UwU

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + 'index.html');
 });
 
 io.on('connection', function(socket){
